b2fb6979e3878e6ec966ff5c0334bcc3d4130c13
chadfennell <libsys@gmail.com>
2015-07-15 15:12:54 -0500

    graduating from example app status

--- app.js
+++ app.js
@@ -26,8 +26,6 @@ app.use('/train', function(req, res){
 })
 
 var server = app.listen(8000, function () {
-  var host = server.address().address;
-  var port = server.address().port;
-  console.log('Example app listening at http://%s:%s', host, port);
+  console.log('Gerbil listening');
 });
 
